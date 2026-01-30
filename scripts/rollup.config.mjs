572720753d9e3f5e3637be83fdfd8322d4a5af03
Phil Pluckthun <phil@kitten.sh>
2026-01-30 02:41:57 +0000

    chore: Update rollup config to exclude sources from sourcemaps (#17)

--- scripts/rollup.config.mjs
+++ scripts/rollup.config.mjs
@@ -93,7 +93,7 @@ const commonOutput = {
   dir: './',
   exports: 'auto',
   sourcemap: true,
-  sourcemapExcludeSources: false,
+  sourcemapExcludeSources: true,
   hoistTransitiveImports: false,
   indent: false,
   freeze: false,
@@ -186,6 +186,7 @@ const outputPlugins = [
     keep_fnames: true,
     ie8: false,
     compress: {
+      passes: 2,
       pure_getters: true,
       toplevel: true,
       booleans_as_integers: false,
@@ -197,6 +198,7 @@ const outputPlugins = [
       loops: false,
       conditionals: false,
       join_vars: false,
+      reduce_vars: true,
     },
     mangle: {
       module: true,
@@ -222,8 +224,9 @@ export default [
         exclude: 'node_modules/**',
         presets: [],
         plugins: [
-          '@babel/plugin-transform-typescript',
-          '@babel/plugin-transform-block-scoping',
+          ['@babel/plugin-transform-typescript', {
+            optimizeConstEnums: true,
+          }],
         ],
       }),
     ],
