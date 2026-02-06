8504d8bbfe35f5cddeb6180d3e90ed79cd3941eb
Phil Pluckthun <phil@kitten.sh>
2026-02-06 09:38:41 +0000

    Add `noProbe` and `noDhcp` configuration options (#18)

--- src/subprocess.ts
+++ src/subprocess.ts
@@ -1,7 +1,10 @@
 import { lanNetwork } from './index';
 
 async function output() {
-  const assignment = await lanNetwork();
+  const assignment = await lanNetwork({
+    noProbe: process.argv.includes('--no-probe'),
+    noDhcp: process.argv.includes('--no-dhcp'),
+  });
   process.stdout.write(JSON.stringify(assignment));
   process.exit(0);
 }
